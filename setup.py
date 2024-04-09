75d0b3b774e1812c028b3205bd9b1ea0bc606433
Roman Zeyde <roman.zeyde@vastdata.com>
2024-04-09 17:22:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,33 +2,12 @@ import os
 from setuptools import setup, find_packages
 
 long_description = """
-`VastdbApi` is a Python based API designed for interacting with *VastDB* & *Vast Catalog*, enabling operations such as schema and table management, data querying, and transaction handling.
-Key libraries used in this API include requests for HTTP requests, pyarrow for handling Apache Arrow data formats, and flatbuffers for efficient serialization of data structures.
+`vastdb` is a Python-based SDK designed for interacting
+with [VAST Database](https://vastdata.com/database)
+and [VAST Catalog](https://vastdata.com/blog/vast-catalog-treat-your-file-system-like-a-database),
+enabling schema and table management, efficient ingest, query and modification of columnar data.
 
-
-```
-pip install vastdb
-```
-
-## Creating the initial session with VastdbApi:
-
-```python
-from vastdb import api
-import pyarrow as pa
-import vast_flatbuf
-from vastdb.api import VastdbApi
-
-def create_vastdb_session(access_key, secret_key):
-    return VastdbApi(host='VAST_VIP_POOL_DNS_NAME', access_key=access_key, secret_key=secret_key)
-
-
-access_key='D8UDFDF...'
-secret_key='B7bqMegmj+TDN..'
-vastdb_session = create_vastdb_session(access_key, secret_key)
-
-```
-#### For the complete Guide for the SDK please go to VastData github: https://github.com/vast-data/vastdb_sdk
-
+For more details, see [our whitepaper](https://vastdata.com/whitepaper/#TheVASTDataBase).
 """
 
 def _get_version_suffix():
@@ -46,7 +25,7 @@ setup(
     name='vastdb',
     python_requires='>=3.9.0',
     description='VAST Data SDK',
-    version='0.0.5.3' + suffix,
+    version='0.1.0-rc1' + suffix,
     url='https://github.com/vast-data/vastdb_sdk',
     author='VAST DATA',
     author_email='hello@vastdata.com',
@@ -55,4 +34,15 @@ setup(
     install_requires=open('requirements.txt').read().strip().split('\n'),
     long_description=long_description,
     long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Topic :: Database',
+        'Topic :: Database :: Front-Ends',
+    ],
 )
