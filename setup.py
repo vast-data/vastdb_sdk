27802692f678f443c0b3d8033eec0991ef866d9c
Roman Zeyde <roman.zeyde@vastdata.com>
2024-04-10 12:17:32 +0300

    Use `0.1.0rc1` as the upcoming version
    We are releasing SDK beta soon.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name='vastdb',
     python_requires='>=3.9.0',
     description='VAST Data SDK',
-    version='0.1.0-rc1' + suffix,
+    version='0.1.0rc1' + suffix,
     url='https://github.com/vast-data/vastdb_sdk',
     author='VAST DATA',
     author_email='hello@vastdata.com',
@@ -35,7 +35,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.9',
