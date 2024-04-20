ae462ab3e5a8354ce6ce005b4735c7ea0c5f1d7b
Roman Zeyde <roman.zeyde@vastdata.com>
2024-04-20 19:47:37 +0300

    Use functools.wraps() for vastdb.connect

--- vastdb/__init__.py
+++ vastdb/__init__.py
@@ -1,7 +1,11 @@
 """VAST Database Python SDK."""
 
+import functools
+
 from . import session
 
+
 # A helper function, useful as a short-hand for Session c-tor: `session = vastdb.connect(...)`
-connect = session.Session
-connect.__name__ = 'connect'
+@functools.wraps(session.Session)
+def connect(*args, **kwargs): # noqa: D103
+    return session.Session(*args, **kwargs)
