2e67ad1f5136f9711c8e5791f1bb60ab50225b42
Roman Zeyde <roman.zeyde@vastdata.com>
2024-04-23 14:04:07 +0300

    Remove verbose logging during test

--- vastdb/tests/util.py
+++ vastdb/tests/util.py
@@ -11,7 +11,6 @@ def prepare_data(session, clean_bucket_name, schema_name, table_name, arrow_tabl
         t = s.create_table(table_name, arrow_table.schema)
         row_ids_array = t.insert(arrow_table)
         row_ids = row_ids_array.to_pylist()
-        log.debug("row_ids=%s" % row_ids)
         assert row_ids == list(range(arrow_table.num_rows))
         yield t
         t.drop()
