7c4c2b27f025196703dc6c7839353f6d83bdc0ea
Roman Zeyde <roman.zeyde@vastdata.com>
2024-07-15 19:37:25 +0300

    Use unique prefix for docs tarball

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,16 +4,22 @@ export VASTDB_APPEND_VERSION_SUFFIX=true
 
 python3 --version
 python3 setup.py sdist bdist_wheel
-cd dist
 
-ls -lh
-sha256sum * | tee SHA256SUMS
-
+pushd dist
 pip3 install *.whl
 pip3 freeze
+unique_build=$(ls -1 *.whl | sed 's/.vast_.*//g')
+unique_build=${unique_build%-py3-none-any.whl}
+echo "unique_build = $unique_build"
+popd
 
-cd ../docs
+pushd docs
 pip3 install -r requirements.txt
 rm -rf build/
 sphinx-build -b html source/ build/html
-tar cvfz ../dist/docs-html.tgz -C build html
+tar cvfz ../dist/${unique_build}-docs-html.tgz -C build html
+popd
+
+pushd dist
+ls -lh
+sha256sum * | tee SHA256SUMS
