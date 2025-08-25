90093d50e8cdc7ba407a69ee8c1ee8535ba7862d
ido.silverwater <ido.silverwater@vastdata.com>
2025-08-25 11:57:01 +0000

    fix: nix shell and direnv

--- shell.nix
+++ shell.nix
@@ -2,24 +2,23 @@
 
 let
   pkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/tarball/dfec583872cf7ae087a5ff8659e6480798f16ba9";
-    sha256 = "sha256:02mmzm1cc06ma0m0bmdx2awn208vb54yf9mcda6k3g2znzjr7lny";
+    url = "https://github.com/NixOS/nixpkgs/tarball/nixpkgs-unstable";
   }) { };
 in pkgs.mkShellNoCC {
+  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+
   packages = (with pkgs; [
+    stdenv.cc.cc.lib
+
+    openssl
+    pkg-config
+
     pyright
     ruff
 
-    python311
-    python311Packages.numpy
-    python311Packages.pyarrow
-    python311Packages.autopep8
-    python311Packages.ibis-framework
-    python311Packages.urllib3
-    python311Packages.flatbuffers
-    python311Packages.backoff
-    python311Packages.pytest
-    python311Packages.flaky
+    python312
+    python312Packages.autopep8
+    python312Packages.flatbuffers
   ]);
 }
 
